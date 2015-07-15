e0ae742a86d046bd3096b3678b221239d7c0a14d
Mohamed Abdallah <abdallam@b-virtual.org>
2015-07-15 18:26:13 +0200

    Basic config parsing.

--- tatts_scrap/analyze_it.py
+++ tatts_scrap/analyze_it.py
@@ -1,4 +1,15 @@
 from sqlite_db import SQLITE_DB
+from ConfigParser import ConfigParser
+
+TATTS_ANALYSIS_CFG = "tatts_analysis.cfg"
+global FILTERS
+
+FILTERS = {"tipsters_performance": {"box_number": "N/A", "tipster_name": "N/A",
+                                               "tipster_id": "N/A", "venue": "N/A",
+                                               "start_date": "N/A", "end_date": "N/A",
+                                                "distance_minimum": "N/A",
+                                                "distance_maximum": "N/A",
+                                                "tf_pool_size": "N/A", "number_of_runners": "N/A"}}
 
 
 def validate_user_input(user_input):
@@ -16,11 +27,32 @@ def validate_user_input(user_input):
 
 
 def tipsters_performance():
+    global FILTERS
     print "tipster performance method"
+    parse_config_update_filters(file_path=TATTS_ANALYSIS_CFG, section_name="tipsters_performance")
+
+    for key, value in FILTERS.iteritems():
+        print "%s = %s" % (key, value)
 
 
 def winning_box():
     print "winning box method"
+
+
+def parse_config_update_filters(file_path, section_name):
+    """
+    This method will read the config file and return key value pair.
+    """
+    global FILTERS
+    config = ConfigParser()
+    config.read(file_path)
+    for section in config.sections():
+        print "section = ", section
+        if section in FILTERS.keys():
+            for option in config.options(section):
+                print "option = ", option
+                print "option value = ", config.get(section, option)
+                FILTERS[section] = config.get(section, option)
 
 
 def analyze_it():
